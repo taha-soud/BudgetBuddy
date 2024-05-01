5e4a8319bf7c0ece96c38ddd770008920c2dd80f
Hp <abuasloudt@gmail.com>
2024-05-01 17:17:17 +0300

    splash screen and app icon added

---
+++ lib/splash_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'dart:async'; // Required for using Timer
+import 'main.dart';
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 2), () {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => MyHomePage(title: 'Home Page')));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.teal,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Icon(Icons.account_balance_wallet, size: 100, color: Colors.white), // An example icon
+            Text('Welcome to Budget Buddy',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white)),
+            Padding(
+              padding: EdgeInsets.only(top: 20),
+
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
