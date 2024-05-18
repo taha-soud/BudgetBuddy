53911acede4cb01f8931932176b6a22f311d9865
Izz-Moqbel <116845081+Izz-Moqbel@users.noreply.github.com>
2024-05-18 18:42:19 +0300

    design category page

---
+++ lib/services/category_service.dart
@@ -0,0 +1,28 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../models/category_model.dart';
+
+
+class CategoryService {
+  final CollectionReference _categoriesCollection = FirebaseFirestore.instance.collection('category');
+
+
+  Future<Category?> getCategoryById(String id) async {
+    final docSnapshot = await _categoriesCollection.doc(id).get();
+    if (docSnapshot.exists) {
+      return Category.fromJson(docSnapshot.data() as Map<String, dynamic>);
+    }
+    return null;
+  }
+
+
+  Future<List<Category>> getCategoriesByUserId(String userId) async {
+    final querySnapshot = await _categoriesCollection.where('userId', isEqualTo: userId).get();
+    return querySnapshot.docs.map((doc) => Category.fromJson(doc.data() as Map<String, dynamic>)).toList();
+  }
+
+  Future<List<Category>> getCategoriesByType(String type) async {
+    final querySnapshot = await _categoriesCollection.where('type', isEqualTo: type).get();
+    return querySnapshot.docs.map((doc) => Category.fromJson(doc.data() as Map<String, dynamic>)).toList();
+  }
+
+}
--- lib/views/category_screen.dart
+++ lib/views/category_screen.dart
@@ -1,34 +1,126 @@
+import 'package:budget_buddy/views/add_category_screen.dart';
+import 'package:budget_buddy/views/expense_screen.dart';
 import 'package:flutter/material.dart';
-
+import '../models/category_model.dart';
 import '../res/custom_color.dart';
+import '../services/category_service.dart';
+import '../utils/icons.dart';
 
 class CategoryScreen extends StatelessWidget {
   const CategoryScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // Define the list of category types
+    final List<String> categoryTypes = ['Food and Drink', 'Transportation', 'Lifestyle', 'My Categorises'];
+
     return Scaffold(
       backgroundColor: AppColors.primary,
       appBar: AppBar(
         backgroundColor: AppColors.primary,
-        title: const Text('Category', style: TextStyle(color: Colors.white)),
+        title: const Text('Category', style: TextStyle(color: AppColors.secondary)),
         centerTitle: true,
         leading: IconButton(
-          icon: const Icon(Icons.arrow_back, color: Colors.white),
+          icon: const Icon(Icons.arrow_back, color: AppColors.secondary),
           onPressed: () {
-            Navigator.of(context).pop();
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => const ExpenseScreen()
+              ),
+            );
           },
         ),
         actions: <Widget>[
           IconButton(
-            icon: const Icon(Icons.add, color: Colors.white),
+            icon: const Icon(Icons.add, color: AppColors.secondary),
             onPressed: () {
-              // Action when notification icon is pressed
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const AddCategoryScreen()
+                ),
+              );
             },
           )
         ],
       ),
-
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: List.generate(categoryTypes.length, (index) {
+              final String categoryType = categoryTypes[index];
+              return FutureBuilder<List<Category>>(
+                future: CategoryService().getCategoriesByType(categoryType),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}'));
+                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                    return const SizedBox();
+                  } else {
+                    final List<Category> categories = snapshot.data!;
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(height: 16.0),
+                        Text(
+                          categoryType,
+                          style: const TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                            color: AppColors.secondary,
+                          ),
+                        ),
+                        const Divider(
+                          color: AppColors.secondary,
+                          thickness: 2.0,
+                        ),
+                        GridView.builder(
+                          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 4,
+                            mainAxisSpacing: 8.0,
+                            crossAxisSpacing: 16.0,
+                            childAspectRatio: 0.8,
+                          ),
+                          itemCount: categories.length,
+                          shrinkWrap: true,
+                          physics: const NeverScrollableScrollPhysics(),
+                          itemBuilder: (context, index) {
+                            final Category category = categories[index];
+                            final IconData iconData = getIconData(category.icon);
+                            return GestureDetector(
+                              onTap: () {
+                                // Handle button tap here
+                                print('${category.name} button tapped');
+                              },
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(iconData, color: AppColors.secondary, size: 36.0),
+                                  const SizedBox(height: 4.0),
+                                  Text(
+                                    category.name,
+                                    style: const TextStyle(color: AppColors.secondary),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
+                        ),
+                      ],
+                    );
+                  }
+                },
+              );
+            }),
+          ),
+        ),
+      ),
     );
   }
 }
