dccc48e3d75a6dc6b3f9e51e49cd5fea49d44a10
Mosab01 <mosabkhraim3@gmail.com>
2024-05-22 15:32:58 +0300

    homePage: transaction card completed both design and logic

--- lib/view_models/transactionSubCard_viewmodel.dart
+++ lib/view_models/transactionSubCard_viewmodel.dart
@@ -1,44 +1,64 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
+import '../models/transaction_model.dart';
+import '../models/category_model.dart';
 
-class TransactionViewModel extends ChangeNotifier {
-  String title = '';
-  String description = '';
-  String amount = '';
-  String time = '';
+class HomeViewModel {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  Future<void> fetchLastTransaction() async {
+  Future<List<Transactions>> fetchLastTwoTransactions() async {
+    final user = _auth.currentUser;
+    if (user == null) {
+      return [];
+    }
+
     try {
-      User? user = FirebaseAuth.instance.currentUser;
+      final querySnapshot = await _firestore
+          .collection('transaction')
+          .where('userId', isEqualTo: user.uid)
+          .orderBy('date', descending: true)
+          .limit(2)
+          .get();
 
-      if (user != null) {
-        print('Fetching last transaction for user: ${user.uid}');
+      return querySnapshot.docs
+          .map((doc) => Transactions.fromJson(doc.data()))
+          .toList();
+    } catch (e) {
+      print('Error fetching transactions: $e');
+      return [];
+    }
+  }
 
-        QuerySnapshot querySnapshot = await FirebaseFirestore.instance
-            .collection('transaction')
-            .where('userId', isEqualTo: user.uid) // Use user's actual ID here
-            .orderBy('date', descending: true)
-            .limit(1)
-            .get();
-
-        if (querySnapshot.docs.isNotEmpty) {
-          DocumentSnapshot doc = querySnapshot.docs.first;
-          Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
-
-          print('Fetched transaction data:');
-          print('Description: ${data['description']}');
-          print('Amount: ${data['amount']}');
-          print('Time: ${data['time']}');
-
-          description = data['description'];
-          amount = data['amount'];
-          time = data['time'];
-          notifyListeners();
-        } else {}
+  Future<Category> fetchCategoryById(String categoryId) async {
+    try {
+      final categorySnapshot =
+          await _firestore.collection('category').doc(categoryId).get();
+      if (categorySnapshot.exists) {
+        final data = categorySnapshot.data();
+        return Category(
+          id: data?['id'] ?? 'unknown',
+          userId: data?['userId'] ?? '',
+          name: data?['name'] ?? 'Unknown',
+          type: data?['type'] ?? 'unknown',
+          icon: data?['icon'] ?? 'Icons.error',
+        );
+      } else {
+        return Category(
+            id: 'unknown',
+            userId: '',
+            name: 'Unknown',
+            type: 'unknown',
+            icon: 'Icons.error');
       }
     } catch (e) {
-      print('Error fetching data: $e');
+      print('Error fetching category: $e');
+      return Category(
+          id: 'unknown',
+          userId: '',
+          name: 'Unknown',
+          type: 'unknown',
+          icon: 'Icons.error');
     }
   }
 }
